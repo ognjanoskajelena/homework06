815c363ba6d2d8939c447b395fac7a2c7180af83
Jelena Ognjanoska <jognjanoska@yahoo.com>
2021-07-29 02:58:29 +0200

    Modify controllers and frontend files (html and css)

--- src/main/java/mk/ukim/finki/hci/homework06/web/DashboardController.java
+++ src/main/java/mk/ukim/finki/hci/homework06/web/DashboardController.java
@@ -3,7 +3,7 @@ package mk.ukim.finki.hci.homework06.web;
 import mk.ukim.finki.hci.homework06.model.Participant;
 import mk.ukim.finki.hci.homework06.model.User;
 import mk.ukim.finki.hci.homework06.service.UserService;
-//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +22,7 @@ public class DashboardController {
         this.userService = userService;
     }
 
-//    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_INITIATOR','ROLE_PARTICIPANT')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_INITIATOR','ROLE_PARTICIPANT')")
     @GetMapping
     public String getDashboardPage(HttpServletRequest request, Model model) {
         Optional<User> optionalUser = this.userService.findByUsername(request.getRemoteUser());
--- src/main/java/mk/ukim/finki/hci/homework06/web/LoginController.java
+++ src/main/java/mk/ukim/finki/hci/homework06/web/LoginController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
     @GetMapping
-    public String getLoginPage(Model model) {
+    public String getLoginPage(@RequestParam(required = false) String error, Model model) {
+        if(error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
         model.addAttribute("headTitle", "Login Page");
         model.addAttribute("style1", "header.css");
         model.addAttribute("style2", "login.css");
